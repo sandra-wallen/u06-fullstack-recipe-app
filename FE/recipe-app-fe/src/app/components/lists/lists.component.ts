e878e07552e25e5670c1907656a8b8d1747f754a
Sandra Wallén <75799823+sandra-wallen@users.noreply.github.com>
2022-06-18 22:12:39 +0200

    Redirect to /login if user is not loged in. Catch errors and render in template. Add functionality to create list

--- FE/recipe-app-fe/src/app/components/lists/lists.component.ts
+++ FE/recipe-app-fe/src/app/components/lists/lists.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/shared/auth.service';
 import { ListService } from 'src/app/shared/list.service';
 import { RecipeService } from 'src/app/shared/recipe.service';
@@ -10,26 +12,61 @@ import { RecipeService } from 'src/app/shared/recipe.service';
 })
 export class ListsComponent implements OnInit {
 
+  form!: FormGroup;
   lists!: any;
+  getListsErr: boolean = false;
+  getListsErrMsg: string = '';
+  postListErr: boolean = false;
+  postListErrMsg: string = '';
 
   constructor(
     private authService: AuthService, 
-    private recipeService: RecipeService, 
-    private listService: ListService
+    private listService: ListService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
 
+    if (!this.authService.isLoggedIn) {
+      this.router.navigate(['/login']);
+    }
+
     const token = this.authService.getToken;
-    this.listService.getLists(token()).subscribe((data: any) => {
-      this.lists = data;
-      console.log(this.lists)
+    this.listService.getLists(token()).subscribe({
+      next: res => {
+        this.lists = res;
+      },
+      error: err => {
+        if (err.status === 404) {
+          this.getListsErr = true;
+          this.getListsErrMsg = err.error.message;
+        }
+      }
+    })
+
+    this.form = new FormGroup({
+      title: new FormControl('')
+    })
+  }
+
+  onSubmit(event: Event): void {
+    event.preventDefault();
+    const title = this.form.value.title;
+
+    const token = this.authService.getToken;
+    this.listService.createList(token(), title).subscribe({
+      next: res => {
+        this.lists = [...this.lists, res];
+      },
+      error: err => {
+        this.postListErr = true;
+        this.postListErrMsg = err.error.message;
+      }
     })
   }
 
   recipesArrCount(arr: string): Array<string> {
-    const recipes = JSON.parse(arr);
-    return recipes !== null ? recipes.length : '0';
+    return arr !== null ? JSON.parse(arr).length : '0';
   }
 
 }
