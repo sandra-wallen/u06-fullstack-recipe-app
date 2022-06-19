482dba1cad1f9f76629759f002961de0431f112c
Sandra Wallén <75799823+sandra-wallen@users.noreply.github.com>
2022-06-19 22:06:50 +0200

    Update environment variables

--- FE/recipe-app-fe/src/environments/environment.prod.ts
+++ FE/recipe-app-fe/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  API_URL: 'https://u06-backend-sandrawallen.herokuapp.com/api',
+  RECIPE_API_ID: '1e4ecae3',
+  RECIPE_API_KEY: '%208e3b71f7b15b3a51a6f4e1b82cdf2d60'
 };
--- FE/recipe-app-fe/src/environments/environment.ts
+++ FE/recipe-app-fe/src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  API_URL: 'http://localhost:8000/api',
+  RECIPE_API_ID: '1e4ecae3',
+  RECIPE_API_KEY: '%208e3b71f7b15b3a51a6f4e1b82cdf2d60'
 };
 
 /*
